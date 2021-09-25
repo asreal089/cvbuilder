d7480272d681cbc9ea6eec83952cd5309dcbba1c
Alexandre Siqueira <mark80@MacBook-Air-de-Alexandre.local>
2021-09-24 22:51:46 -0300

    add bootstrap, rotas e links

---
+++ pages/dicas.tsx
@@ -0,0 +1,9 @@
+import { NextPage } from "next";
+
+const dicas:NextPage = () =>{
+    return (
+        <h1>pagina com dicas sobre como montar o seu currículo.</h1>
+    )
+}
+
+export default dicas;
---
+++ pages/importante.tsx
@@ -0,0 +1,9 @@
+import { NextPage} from "next";
+
+const importante:NextPage = () =>{
+    return(
+        <h1>Importante:</h1>
+    )
+}
+
+export default importante;
