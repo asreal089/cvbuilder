2e113874d2d5c55e0cf20d5c7d0a7dc5ab3e77d0
asreal089 <lexx_89@hotmail.com>
2022-05-20 07:27:30 -0300

    Salvando requisições

--- pages/builder.tsx
+++ pages/builder.tsx
@@ -1,12 +1,11 @@
 import { NextPage } from "next";
-import DadosLinks from "./components/links";
 import React, {
   Component,
   FunctionComponent,
   ReactElement,
   useState,
 } from "react";
-import { Conquistas, Curso, Experiencia } from "../util/models/types";
+import { Conquistas, Curso, Experiencia, Cv } from "../util/models/types";
 import {
   Container,
   Button,
@@ -15,39 +14,66 @@ import {
   TextField,
 } from "@material-ui/core";
 import AddCircleIcon from "@mui/icons-material/AddCircle";
-import { inputClasses } from "@mui/material";
+import router from "next/dist/client/router";
 
 const Builder: NextPage = () => {
-  
-  const [dadosLinkList, setDadoslinkList] = useState<any>([]);
-  const [nome, setNome] = useState<String>();
-  const [localidade, setLocalidade] = useState<String>();
-  const [email, setEmail] = useState<String>();
+  const [nome, setNome] = useState<string>("");
+  const [localidade, setLocalidade] = useState<string>("");
+  const [email, setEmail] = useState<string>("");
   const [titulo_palavras_chave, settitulo_palavras_chave] =
     useState<[String]>();
   const [links, setLinks] = useState<[String]>();
   const [cover_letter, setCover_letter] = useState<String>();
-  const [habilidades, setHabilidades] = useState<any>(['']);
+  const [habilidades, setHabilidades] = useState<any>([""]);
   const [experiencia, setExperiencia] = useState<Experiencia>();
   const [cursos, setCursos] = useState<Curso>();
   const [conquistas, setConquistas] = useState<Conquistas>();
 
-  function saveCV() {
-    console.log("Nome: " + nome);
-    console.log("email: " + email);
-    console.log("localidade: " + localidade);
-    console.log("habilidades: " + habilidades);
+  async function saveCV() {
+    var cv = buildCV();
+    const hostname = window.location.hostname;
+    await fetch("/api/cv", {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(cv),
+    }).then(res => {
+      if(res.status == 200){
+        alert("CV salvo")
+      }
+      console.log(res)
+    })
+    router.push("/")
   }
 
+  function buildCV() {
+    var cv: Cv = {
+      nome: nome,
+      localidade: localidade,
+      email: email,
+      titulo_palavras_chave: [],
+      links: [],
+      cover_letter: "",
+      habilidades: [],
+      experiencia: [],
+      cursos: [],
+      conquistas: [],
+    };
+    return cv;
+  }
   function pushHabilidade() {
-    let novaHabilidade = '';
-    setHabilidades([ '' ,...habilidades]);
+    let novaHabilidade = "";
+    setHabilidades(["", ...habilidades]);
   }
 
-  function setNovaHabilidade(e: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>, index: number){
-      let temp= habilidades.map((i: any)=>i);
-      temp[index] = e.target.value;
-      setHabilidades(temp);
+  function setNovaHabilidade(
+    e: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>,
+    index: number
+  ) {
+    let temp = habilidades.map((i: any) => i);
+    temp[index] = e.target.value;
+    setHabilidades(temp);
   }
 
   return (
@@ -105,23 +131,23 @@ const Builder: NextPage = () => {
           }}
         />
         Habilidades:
-        
         {habilidades.map((_item: any, index: number) => (
-            <>
-              <br />
-              <TextField
-                name="habilidade"
-                key={"habilidade"}
-                className="habilidade"
-                type="text"
-                label="habilidade"
-                variant="outlined"
-                required
-                onChange={(e) => {
-                  setNovaHabilidade(e, index);
-                } } />
-            </>
-          ))}
+          <>
+            <br />
+            <TextField
+              name="habilidade"
+              key={"habilidade"}
+              className="habilidade"
+              type="text"
+              label="habilidade"
+              variant="outlined"
+              required
+              onChange={(e) => {
+                setNovaHabilidade(e, index);
+              }}
+            />
+          </>
+        ))}
         <br />
         <span className="center">
           <Button variant="contained" color="primary" onClick={pushHabilidade}>
--- util/models/types.ts
+++ util/models/types.ts
@@ -1,43 +1,38 @@
 export interface ResponseFuncs {
-    GET?: Function
-    POST?: Function
-    PUT?: Function
-    DELETE?: Function
-  }
-  
+  GET?: Function;
+  POST?: Function;
+  PUT?: Function;
+  DELETE?: Function;
+}
 
-  export interface Cv {
-    _id?: number
-    nome: string
-    titulo_palavras_chave: string[]
-    localidade: string
-    email: string
-    links: string[]
-    cover_letter: string
-    habilidades: string[]
-    experiencia: Experiencia[]
-    cursos: Curso[]
-    conquistas: Conquistas[]
-    
-  }
+export interface Cv {
+  _id?: string;
+  nome: string;
+  titulo_palavras_chave: string[];
+  localidade: string;
+  email: string;
+  links: string[];
+  cover_letter: string;
+  habilidades: string[];
+  experiencia: Experiencia[];
+  cursos: Curso[];
+  conquistas: Conquistas[];
+}
 
-  export interface Experiencia { 
-    empresa: string
-    incio: string
-    fim: string
-    descricao: string
-   
-  }
-  
-  export interface Curso {
-    instituicao: string
-    duracao: string
-    descricao: string   
+export interface Experiencia {
+  empresa: string;
+  incio: string;
+  fim: string;
+  descricao: string;
+}
 
-  }
+export interface Curso {
+  instituicao: string;
+  duracao: string;
+  descricao: string;
+}
 
-  export interface Conquistas{
-    titulo: string
-    descricao: string
-
-  }
+export interface Conquistas {
+  titulo: string;
+  descricao: string;
+}
